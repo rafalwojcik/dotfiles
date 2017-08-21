415b9a6b19e73186f4e572cf6719fa8f96df5571
Rafał Wójcik <rafalwojcik@me.com>
2017-08-21 13:31:52 +0200

    Update bluetooth sound quality

--- my-tools.sh
+++ my-tools.sh
@@ -26,3 +26,12 @@ cp ./sources/XcodeThemes/* ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 git config --global user.name "Rafał Wójcik"
 git config --global user.email rafalwojcik@me.com
 git config --global merge.tool opendiff
+
+# Update blouetootsh sound quality
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool Min (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" 80
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" 80
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" 80
